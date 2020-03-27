4e634b1762b5b059bb913f3eb10765bb26b30642
kalkaru <lara.faelivrin@gmail.com>
2020-03-27 17:57:26 +0100

    update prepareData.R script and add resulting csv

--- prepareData.R
+++ prepareData.R
@@ -4,6 +4,35 @@
 # Date: 24.03.2020
 ###############################################################################
 
-file <- 'base_data_IBE.csv'
+# Import libraries
+library(dplyr) # Version: ‘0.8.5’
+library(tidyr) # ‘1.0.2’
 
-data_raw <- read.csv(file, as.is = TRUE)
+# Number formatting
+options(scipen = 1000000)
+options(digits = 6)
+
+# import dat
+url_dat  <- "https://raw.githubusercontent.com/bildungsmonitoringZH/covid19_edu_mindsteps/master/base_data_IBE.csv?token=AJX6OINGRDZVQ4TGPHEYQ3K6Q5TJS"
+dat <- read.csv(url(url_dat), header=T, sep=",", stringsAsFactors=FALSE, encoding="UTF-8")
+
+#prepare dat
+dat_prep <- dat %>%
+  mutate(
+    value = n,
+    topic = "Bildung",
+    variable_short = "training_mindsteps",
+    variable_long = "Nutzung der Lernplattform Mindsteps",
+    location = "Deutschschweiz",
+    unit= "Anzahl durchgeführter Aufgabenserien",
+    source = "Universität Zürich, Institut für Bildungsevaluation",
+    update = "täglich",
+    public = "ja",
+    description = "https://github.com/bildungsmonitoringZH/covid19_edu_mindsteps"
+  ) %>%
+  select(-region, -comment,) %>%
+  select(date, value, topic, variable_short, variable_long, location, unit, source, update, public, description) 
+
+# export dat
+write.table(dat_prep, "./Bildung_LernplattformMindsteps.csv", sep=",", fileEncoding="UTF-8", row.names = F)
+
